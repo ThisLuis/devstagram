a875997213c8bc5ab841ffeb1c5ef0390e72f550
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2022-12-15 15:06:48 -0600

    Se agregaron los elementos finales para el template de create post, estan listos los inputs y el boton, solo falta agregar Drop Zone para terminar con la interfaz y continuar con la funcionaliad

--- resources/views/posts/create.blade.php
+++ resources/views/posts/create.blade.php
@@ -13,12 +13,12 @@
             <form action="{{ route('register') }}" method="POST">
                 @csrf
                 <div class="mb-5">
-                    <label class="mb-2 block uppercase text-gray-500 font-bold" for="name">Name</label>
+                    <label class="mb-2 block uppercase text-gray-500 font-bold" for="title">Title</label>
                     <input
-                        id="name"
-                        name="name"
+                        id="title"
+                        name="title"
                         type="text"
-                        placeholder="Your name"
+                        placeholder="Title"
                         class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
                         value="{{ old('name') }}"
                     >
@@ -29,6 +29,27 @@
                         </p>
                     @enderror
                 </div>
+
+                <div class="mb-5">
+                    <label class="mb-2 block uppercase text-gray-500 font-bold" for="description">Describe</label>
+                    <textarea
+                        id="description"
+                        name="description"
+                        placeholder="Type description"
+                        class="border p-3 w-full rounded-lg @error('name') border-red-500 @enderror"
+                    >{{ old('name') }}</textarea>
+                    {{-- En 'name' va el nombre del campo que vamos a validar --}}
+                    @error('name')
+                        <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center">
+                            {{ $message }}
+                        </p>
+                    @enderror
+                </div>
+                <input
+                    type="submit"
+                    value="Publish"
+                    class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                >
             </form>
         </div>
     </div>
