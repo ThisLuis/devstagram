bbaa01303cb8d9aa205d72d89ea9c7910c6df24b
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2022-12-23 09:08:00 -0600

    Creacion de una caja de comentarios en show.blade.php

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -25,8 +25,33 @@
             </div>
 
         </div>
-        <div class="md:w-1/2">
-            2
+        <div class="md:w-1/2 p-5">
+            <div class="shadow bg-white p-5 mb-5">
+                <p class="text-xl font-bold text-center mb-4">Agrega un nuevo comentario</p>
+                <form action="">
+                    <div class="mb-5">
+                        <label for="comment" class="mb-2 block uppercase text-gray-500 font-bold">
+                            Comment
+                        </label>
+                        <textarea 
+                            name="comment" 
+                            id="comment" 
+                            placeholder="Type comment"
+                            class="border p-3 w-full rounded-lg @error('') border-red-500 @enderror"
+                        >
+                            
+                        </textarea>
+                        @error('comment')
+                            <p class="bg-red-500 text-white my-2 rounded-lg text-sm p-2 text-center"></p>
+                        @enderror
+                    </div>
+                    <input 
+                        type="submit"
+                        value="Comment"
+                        class="bg-sky-600 hover:bg-sky-700 transition-colors cursor-pointer uppercase font-bold w-full p-3 text-white rounded-lg"
+                    />
+                </form>
+            </div>
         </div>
     </div>
 @endsection
