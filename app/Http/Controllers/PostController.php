68eb52c3933d48217595f7786106e218c02fba53
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2022-12-27 19:31:12 -0600

    Se agrego la funcionaldiad que al borrar un post tambien se elimine la imagen de la carpeta uploads

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\Post;
 use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\File;
 
 class PostController extends Controller
 {
@@ -93,6 +94,14 @@ class PostController extends Controller
         // Entre parentesis va el metodo que queremos ejecutar del policy, en este caso delete
             $this->authorize('delete', $post);
             $post->delete();
+
+            // Eliminar la imagen
+            $image_path = public_path(('uploads/' . $post->image));
+            if(File::exists($image_path))
+            {
+                unlink($image_path);
+            }
+
             return redirect()->route('posts.index', auth()->user()->username);
     }
 
