86c3b9c009d92162c38ca702c6d961525feffc07
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2022-12-29 22:06:59 -0600

    Se creo y esta lista la funcionalidad de editar el perfil de los usuarios, asi como la subida de imagenes a la carpeta profiles

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -2,8 +2,10 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Support\Str;
 use Illuminate\Http\Request;
+use Intervention\Image\Facades\Image;
 
 class ProfileController extends Controller
 {
@@ -25,9 +27,30 @@ class ProfileController extends Controller
         $request->request->add(['username' => Str::slug($request->username)]);
 
         $this->validate($request, [
-            'username' => ['required', 'unique:users, username,'.auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter'],
+            'username' => ['required', 'unique:users,username,'.auth()->user()->id, 'min:3', 'max:20', 'not_in:twitter'],
         ]);
 
-        dd('guardado');
+        if($request->image)
+        {
+            $image = $request->file('image');
+            $nameImage = Str::uuid() . "." . $image->extension();
+    
+            $imageServer = Image::make($image);
+            $imageServer->fit(1000, 1000);
+    
+            $imagePath = public_path('profiles'). '/' . $nameImage;
+
+            $imageServer->save($imagePath);    
+        }
+
+        // Guardamos cambios
+        $user = User::find(auth()->user()->id);
+        $user->username = $request->username;
+        $user->image = $nameImage ?? auth()->user()->image ?? null;
+        $user->save();
+
+        // Para redireccionar pasamos la ultima instancia que es $user, esto nos asegura de traer el username nuevo
+        return redirect()->route('posts.index', $user->username);
+
     }
 }
