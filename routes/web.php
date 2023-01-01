b9f0c5179406455725f6c49402946625bf77c12d
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-01-01 17:18:55 -0600

    Creacion de Migracion, Modelo y Controlador de Follower

--- routes/web.php
+++ routes/web.php
@@ -10,6 +10,7 @@ use App\Http\Controllers\ImageController;
 use App\Http\Controllers\LoginController;
 use App\Http\Controllers\LogoutController;
 use App\Http\Controllers\CommentaryController;
+use App\Http\Controllers\FollowerController;
 use App\Http\Controllers\ProfileController;
 
 /*
@@ -68,6 +69,9 @@ Route::post('/posts/{post}/likes', [LikeController::class, 'store'])->name('post
 
 Route::delete('/posts/{post}/likes', [LikeController::class, 'destroy'])->name('posts.likes.destroy');
 
-
-
+// Siguiendo usuarios
+// Agregar un usuario a nuestros follows
+Route::post('{user:username}/follow', [FollowerController::class, 'store'])->name('users.follow');
+// Eliminar un usuario de nuestros follows
+Route::delete('{user:username}/unfollow', [FollowerController::class, 'destroy'])->name('users.unfollow');
 // Route Model Bainding -> Un modelo esta asociado a una ruta
