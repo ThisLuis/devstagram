6bd3850160186665282d140a5f8d268a8f0c6e07
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-01-02 21:25:58 -0600

    Mostramos la informacion de los likes a la hora de presionar el corazon

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -9,11 +9,13 @@ class LikePost extends Component
     // No tenemos que pasarlo, desde que lo creamos ya esta disponible en la vista
     public $post;
     public $isLiked;
+    public $likes;
 
     // Le pasamos el $post para que sepa cual es la informacion que tiene que procesar
     public function mount($post)
     {
         $this->isLiked = $post->checkLike(auth()->user());
+        $this->likes = $post->likes->count();
     }
 
     public function like()
@@ -23,13 +25,14 @@ class LikePost extends Component
             // request no esta disponible         Usamos this porque es la instancia que se le esta pasando a livewire, ya no es $post, sino $this->post
             $this->post->likes()->where('post_id', $this->post->id)->delete();
             $this->isLiked = false;
-            
+            $this->likes--;
         } else 
         {
             $this->post->likes()->create([
                 'user_id' => auth()->user()->id,
             ]);
             $this->isLiked = true;
+            $this->likes++;
         }
     }
 
--- resources/views/livewire/like-post.blade.php
+++ resources/views/livewire/like-post.blade.php
@@ -1,9 +1,10 @@
 <div>
-    {{-- The whole world belongs to you. --}}
-    {{-- Primer regla, siempre tenemos que retornar un div --}}
-    <button
-        wire:click="like"
-    >
+    
+    <div class="flex gap-2 items-center">
+        {{-- Primer regla, siempre tenemos que retornar un div --}}
+        <button
+            wire:click="like"
+        >
         <svg 
             xmlns="http://www.w3.org/2000/svg" 
             fill="{{ $isLiked ? "red":"white" }}" 
@@ -13,7 +14,12 @@
             class="w-6 h-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M21 8.25c0-2.485-2.099-4.5-4.688-4.5-1.935 0-3.597 1.126-4.312 2.733-.715-1.607-2.377-2.733-4.313-2.733C5.1 3.75 3 5.765 3 8.25c0 7.22 9 12 9 12s9-4.78 9-12z" />
         </svg>
-    </button>
+        </button>
+
+        <p class="font-bold">{{ $likes }} 
+            <span class="font-normal">Likes</span>
+        </p>
+    </div>
 
     
 </div>
