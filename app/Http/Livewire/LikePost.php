47723ef48d3d8b1387ab3d64892a9477cec0f212
Luis Fernando Nieves Nieves <this.luisnieves@gmail.com>
2023-01-02 20:58:28 -0600

    Movimos la logica de Likes y dejar de dar like a LikePost.php con wire y livewire

--- app/Http/Livewire/LikePost.php
+++ app/Http/Livewire/LikePost.php
@@ -11,7 +11,18 @@ class LikePost extends Component
 
     public function like()
     {
-        return "desde la funcion de like";
+        if($this->post->checkLike(auth()->user()))
+        {
+            // request no esta disponible         Usamos this porque es la instancia que se le esta pasando a livewire, ya no es $post, sino $this->post
+            $this->post->likes()->where('post_id', $this->post->id)->delete();
+        } else 
+        {
+            $this->post->likes()->create([
+                'user_id' => auth()->user()->id,
+            ]);
+    
+            return back();
+        }
     }
 
 
